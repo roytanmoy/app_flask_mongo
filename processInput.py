a507ac97593646875126773053fa5347949728f7
tanmoy <tanmoy33@gmail.com>
2018-12-15 16:35:43 +0530

    return value check

--- processInput.py
+++ processInput.py
@@ -1,6 +1,6 @@
 from validation import validatePhoneNumber as vpn
 import csv
-input_file = "aapl.csv"
+input_file = "input_data.csv"
 output_file = "final.csv"
 COUNTRY_CODE = 'ZA'
 
@@ -21,10 +21,13 @@ class processPhoneNumber():
 
     def process_Raw_Data(self, raw_data):
         resp = self.vpn.validate_numbers(raw_data)
+
         for num_type in resp.keys():
-            op_file = num_type+'.csv'
-            op_data = resp[num_type]
-            self.write_to_CSV_File(op_data, op_file = op_file)
+            if resp[num_type]:
+                op_file = num_type+'.csv'
+                op_data = resp[num_type]
+                self.fields = op_data[0].keys()
+                self.write_to_CSV_File(op_data, op_file = op_file)
 
     def write_to_CSV_File(self, op_data, op_file = output_file):
         with open(op_file, 'w') as csvfile:
@@ -32,11 +35,9 @@ class processPhoneNumber():
             csvwriter.writeheader()
             csvwriter.writerows(op_data)
 
-
 if __name__ == "__main__":
     ph_obj = processPhoneNumber()
     raw_data = ph_obj.read_from_CSV_File()
     ph_obj.process_Raw_Data(raw_data)
-    #ph_obj.write_to_CSV_File(op_data)
-    displayData = ph_obj.read_from_CSV_File(output_file)
-    print(displayData)
+    #print (ph_obj.read_from_CSV_File(output_file))
+
